75c9d06cc697440ce64778b9a7dd3fef54df9a53
campos1987 <ewertoncampos@gmail.com>
2025-10-04 09:34:48 -0300

    Projeto funcional

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
+import SelectPage from './function/page';
+
+import Logo from './componentes/Logo';
+import './style.css';
+
+import PyStruct from './json/py.json';
+import JsStruct from './json/js.json';
+import Category from './componentes/Category';
+
+const page = SelectPage();
+const struct = page === 'py' ? PyStruct.phyton : JsStruct.javascript;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <BrowserRouter>
+      <Logo link={page} />
+      <Category link={struct} />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
